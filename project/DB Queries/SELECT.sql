0750b2a67ff30aa4b022316b58d502f3d81e59a4
Muhammed Chreiki <mahmibey@gmail.com>
2026-08-24 16:17:34 +0300

    DB queries directory

---
+++ project/DB Queries/SELECT.sql
@@ -0,0 +1,10 @@
+SELECT * FROM public.command_history;
+SELECT * FROM public.command_steps;
+SELECT * FROM public.custom_commands;
+
+SELECT * FROM public.apps;
+SELECT * FROM public.device_apps;
+SELECT * FROM public.devices;
+
+SELECT * FROM public.error_messages;
+SELECT * FROM public.failed_app_open_attempts;
